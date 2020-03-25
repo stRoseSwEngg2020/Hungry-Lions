bd68d02fd82883232b9ea6be3449528540ba2787
outlawstar80 <60183079+outlawstar80@users.noreply.github.com>
2020-03-25 13:02:30 -0400

    Add files via upload

--- Login.java
+++ Login.java
@@ -30,25 +30,25 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        TreasureHuntPanel = new javax.swing.JPanel();
+        TreasureHuntGameName = new javax.swing.JLabel();
         CloseLoginForm = new javax.swing.JLabel();
         MinimizeLoginForm = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        LoginBackgroundPanel = new javax.swing.JPanel();
+        LoginTextLogo = new javax.swing.JLabel();
+        UsernameTextLogo = new javax.swing.JLabel();
+        PasswordTextLogo = new javax.swing.JLabel();
+        UsernameLoginPageEntry = new javax.swing.JTextField();
+        jLoginPagePasswordField1 = new javax.swing.JPasswordField();
         LoginToGame = new javax.swing.JButton();
         CreateNewUser = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(207, 0, 14));
+        TreasureHuntPanel.setBackground(new java.awt.Color(207, 0, 14));
 
-        jLabel1.setFont(new java.awt.Font("DIN Condensed", 0, 18)); // NOI18N
-        jLabel1.setText("Treasure Hunt");
+        TreasureHuntGameName.setFont(new java.awt.Font("DIN Condensed", 0, 18)); // NOI18N
+        TreasureHuntGameName.setText("Treasure Hunt");
 
         CloseLoginForm.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
         CloseLoginForm.setText("x");
@@ -67,48 +67,48 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout TreasureHuntPanelLayout = new javax.swing.GroupLayout(TreasureHuntPanel);
+        TreasureHuntPanel.setLayout(TreasureHuntPanelLayout);
+        TreasureHuntPanelLayout.setHorizontalGroup(
+            TreasureHuntPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, TreasureHuntPanelLayout.createSequentialGroup()
                 .addContainerGap(181, Short.MAX_VALUE)
-                .addComponent(jLabel1)
+                .addComponent(TreasureHuntGameName)
                 .addGap(122, 122, 122)
                 .addComponent(MinimizeLoginForm)
                 .addGap(18, 18, 18)
                 .addComponent(CloseLoginForm)
                 .addGap(17, 17, 17))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        TreasureHuntPanelLayout.setVerticalGroup(
+            TreasureHuntPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(TreasureHuntPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
+                .addGroup(TreasureHuntPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(TreasureHuntGameName)
                     .addComponent(CloseLoginForm)
                     .addComponent(MinimizeLoginForm))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jPanel2.setBackground(new java.awt.Color(0, 169, 207));
+        LoginBackgroundPanel.setBackground(new java.awt.Color(0, 169, 207));
 
-        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel2.setFont(new java.awt.Font("DIN Condensed", 0, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Login");
+        LoginTextLogo.setBackground(new java.awt.Color(255, 255, 255));
+        LoginTextLogo.setFont(new java.awt.Font("DIN Condensed", 0, 24)); // NOI18N
+        LoginTextLogo.setForeground(new java.awt.Color(255, 255, 255));
+        LoginTextLogo.setText("Login");
 
-        jLabel5.setFont(new java.awt.Font("DIN Condensed", 0, 18)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("Username");
+        UsernameTextLogo.setFont(new java.awt.Font("DIN Condensed", 0, 18)); // NOI18N
+        UsernameTextLogo.setForeground(new java.awt.Color(255, 255, 255));
+        UsernameTextLogo.setText("Username");
 
-        jLabel6.setFont(new java.awt.Font("DIN Condensed", 0, 18)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel6.setText("Password");
+        PasswordTextLogo.setFont(new java.awt.Font("DIN Condensed", 0, 18)); // NOI18N
+        PasswordTextLogo.setForeground(new java.awt.Color(255, 255, 255));
+        PasswordTextLogo.setText("Password");
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        UsernameLoginPageEntry.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                UsernameLoginPageEntryActionPerformed(evt);
             }
         });
 
@@ -128,44 +128,44 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout LoginBackgroundPanelLayout = new javax.swing.GroupLayout(LoginBackgroundPanel);
+        LoginBackgroundPanel.setLayout(LoginBackgroundPanelLayout);
+        LoginBackgroundPanelLayout.setHorizontalGroup(
+            LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(LoginBackgroundPanelLayout.createSequentialGroup()
+                .addGroup(LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(LoginBackgroundPanelLayout.createSequentialGroup()
                         .addGap(192, 192, 192)
-                        .addComponent(jLabel2))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(LoginTextLogo))
+                    .addGroup(LoginBackgroundPanelLayout.createSequentialGroup()
                         .addGap(47, 47, 47)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jLabel6)
-                                    .addComponent(jLabel5))
+                        .addGroup(LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(LoginBackgroundPanelLayout.createSequentialGroup()
+                                .addGroup(LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(PasswordTextLogo)
+                                    .addComponent(UsernameTextLogo))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField1)
-                                    .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 267, Short.MAX_VALUE)))
-                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(UsernameLoginPageEntry)
+                                    .addComponent(jLoginPagePasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 267, Short.MAX_VALUE)))
+                            .addGroup(LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                 .addComponent(LoginToGame, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(CreateNewUser, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        LoginBackgroundPanelLayout.setVerticalGroup(
+            LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(LoginBackgroundPanelLayout.createSequentialGroup()
                 .addContainerGap(20, Short.MAX_VALUE)
-                .addComponent(jLabel2)
+                .addComponent(LoginTextLogo)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel5)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(UsernameTextLogo)
+                    .addComponent(UsernameLoginPageEntry, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(22, 22, 22)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(LoginBackgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(PasswordTextLogo)
+                    .addComponent(jLoginPagePasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(LoginToGame)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -177,23 +177,23 @@ public class Login extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(LoginBackgroundPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(TreasureHuntPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(TreasureHuntPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(LoginBackgroundPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void UsernameLoginPageEntryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UsernameLoginPageEntryActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_UsernameLoginPageEntryActionPerformed
 
     private void CloseLoginFormMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CloseLoginFormMouseClicked
         System.exit(0);
@@ -261,15 +261,15 @@ public class Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel CloseLoginForm;
     private javax.swing.JButton CreateNewUser;
+    private javax.swing.JPanel LoginBackgroundPanel;
+    private javax.swing.JLabel LoginTextLogo;
     private javax.swing.JButton LoginToGame;
     private javax.swing.JLabel MinimizeLoginForm;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel PasswordTextLogo;
+    private javax.swing.JLabel TreasureHuntGameName;
+    private javax.swing.JPanel TreasureHuntPanel;
+    private javax.swing.JTextField UsernameLoginPageEntry;
+    private javax.swing.JLabel UsernameTextLogo;
+    private javax.swing.JPasswordField jLoginPagePasswordField1;
     // End of variables declaration//GEN-END:variables
 }
