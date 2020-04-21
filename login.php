31bf3119073283891c7dbb067c9ac6d24de19a2a
iyolbarsovv <yolbarsovi677@strose.edu>
2020-04-21 17:14:25 -0400

    Updated and fixed

--- login.php
+++ login.php
@@ -1,20 +1,44 @@
-
 <html>
 <head>
 <title>Login</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <body>
 <div class="loginbox"> <img src="https://lh3.googleusercontent.com/BIpeBa7JA4lhYlluvwBc0RnLbMInczIQ47nBEyM_SJ9KP9xBvoL_FLh-Kusx-_8JfF3HIl5lyZc_-ip4dS4nKOW0d9Wyp8UmbVq-9OdklW0hSYD-LvWM4jn_ZHgryRh164uPnx-f=s200-p-k" class="avatar" alt = "avatar logo"/>
   <h1>Login</h1>
-  <form action = "homepage.php" method="post" name="login"
+  <form action = "login.php" method="post" name="login">
     <p>Username</p>
-    <input type="text" id="username" placeholder="Enter Username" required>
+    <input type="text" name="username" placeholder="Enter Username" required>
     <p>Password</p>
-    <input type="password" id="username_Password" placeholder="Enter Password" required>
-    <input type="submit" name="login" value="Login">
+    <input type="password" name="username_Password" placeholder="Enter Password" required>
+    <input type="submit" name="login">
     <a href="register.php">Create account</a>
   </form>
 </div>
 </body>
-</head>
+<?php
+$servername = "localhost";
+$username = "yolbarsovi677";
+$password = "";
+$dbname = "HungryLionsSoftwareEngineProject";
+
+$conn = mysqli_connect( $servername, $username, $password, $dbname );
+
+if ( !$conn ) {
+  die( "Connection failed: " . mysqli_connect_error() );
+}
+
+if ( isset( $_POST[ 'login' ] ) ) {
+  $login_user = mysqli_real_escape_string( $conn, $_REQUEST[ 'username' ] );
+  $login_password = mysqli_real_escape_string( $conn, $_REQUEST[ 'username_Password' ] );
+  $sql = "SELECT * FROM TreasureHuntUsers WHERE username = '$login_user' AND username_Password='$login_password'";
+  $results = mysqli_query( $conn, $sql );
+  if ( mysqli_num_rows( $results ) > 0 ) {
+    header( "Location: homepage.php" );
+  } else {
+    echo "Username or password not found";
+  }
+  mysqli_close( $conn );
+}
+?>
 </html>
